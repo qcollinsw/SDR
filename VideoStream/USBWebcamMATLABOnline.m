70ffd9b7c90b1f983fbd6b428373161fda089850
jackm2983 <j_master101@hotmail.com>
2026-02-03 15:55:12 -0800

    Update USBWebcamMATLABOnline.m
    webcam code

--- VideoStream/USBWebcamMATLABOnline.m
+++ VideoStream/USBWebcamMATLABOnline.m
@@ -1,1 +1,47 @@
 
+%% Setup
+
+% Clear workspace
+clear  
+% Connect to the first webcam
+cam = webcam(1);
+
+%% Single Image
+
+% Take a single image from cam
+img = snapshot(cam);
+% Display image
+imshow(img)       
+
+%% Video
+
+% Loop 30 times
+for frames = 1:30        
+    % Take single image from cam
+    img = snapshot(cam);  
+    % Display image
+    imshow(img)       
+% End loop
+end                         
+
+%% Classification
+
+% Load the pretrained GoogLeNet network
+nnet = googlenet;        
+% Loop 250 times
+for n = 1:250            
+    % Take single image
+    img = snapshot(cam);     
+    % Resize image to 224x224 pixels
+    img = imresize(img, [224, 224]);
+    % Classify image
+    [label, score] = classify(nnet, img);
+    % Display image
+    imshow(img)
+    % Show label, score
+    title({char(label), num2str(max(score),2)});
+% End loop
+end 
+
+
+% Copyright 2019 The MathWorks, Inc.
