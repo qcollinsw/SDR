eec842445657e93d6dbeada9d75b607c5eef31ee
jackm2983 <j_master101@hotmail.com>
2026-02-03 16:09:51 -0800

    Update USBWebcamMATLABOnline.m
    commented out stuff

--- VideoStream/USBWebcamMATLABOnline.m
+++ VideoStream/USBWebcamMATLABOnline.m
@@ -16,7 +16,7 @@ imshow(img)
 %% Video
 
 % Loop 30 times
-for frames = 1:30        
+for frames = 1:300        
     % Take single image from cam
     img = snapshot(cam);  
     % Display image
@@ -26,22 +26,22 @@ end
 
 %% Classification
 
-% Load the pretrained GoogLeNet network
-nnet = googlenet;        
-% Loop 250 times
-for n = 1:250            
-    % Take single image
-    img = snapshot(cam);     
-    % Resize image to 224x224 pixels
-    img = imresize(img, [224, 224]);
-    % Classify image
-    [label, score] = classify(nnet, img);
-    % Display image
-    imshow(img)
-    % Show label, score
-    title({char(label), num2str(max(score),2)});
-% End loop
-end 
+% % Load the pretrained GoogLeNet network
+% nnet = googlenet;        
+% % Loop 250 times
+% for n = 1:250            
+%     % Take single image
+%     img = snapshot(cam);     
+%     % Resize image to 224x224 pixels
+%     img = imresize(img, [224, 224]);
+%     % Classify image
+%     [label, score] = classify(nnet, img);
+%     % Display image
+%     imshow(img)
+%     % Show label, score
+%     title({char(label), num2str(max(score),2)});
+% % End loop
+% end 
 
 
 % Copyright 2019 The MathWorks, Inc.
